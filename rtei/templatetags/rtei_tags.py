6fd3fefea1eb2b6cc8960bd48b44e810b220b7d9
amercader <amercadero@gmail.com>
2016-02-01 16:09:34 +0000

    Add basic top menu and breadcrumbs

---
+++ rtei/templatetags/rtei_tags.py
@@ -0,0 +1,75 @@
+from django import template
+
+from home.models import Page
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+
+def has_menu_children(page):
+
+    # TODO: Use this when we add show_in_menus to the model
+    #return page.get_children().live().in_menu().exists()
+    return page.get_children().live().exists()
+
+
+# Retrieves the top menu items - the immediate children of the parent page
+# The has_menu_children method is necessary because the bootstrap menu requires
+# a dropdown class to be applied to a parent
+@register.inclusion_tag('rtei/tags/top_menu.html', takes_context=True)
+def top_menu(context, parent, calling_page=None):
+
+    # TODO: Use this when we add show_in_menus to the model
+    # menuitems = parent.get_children().live().in_menu()
+    menuitems = parent.get_children().live()
+
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag('rtei/tags/top_menu_children.html', takes_context=True)
+def top_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+
+    # TODO: Use this when we add show_in_menus to the model
+    #menuitems_children = menuitems_children.live().in_menu()
+    menuitems_children = menuitems_children.live()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('rtei/tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    if self is None or self.depth <= 2:
+        # When on the home page, displaying breadcrumbs is irrelevant.
+        ancestors = ()
+    else:
+        ancestors = Page.objects.ancestor_of(
+            self, inclusive=True).filter(depth__gt=2)
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
+    }
