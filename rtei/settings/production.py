3c4035744fe47b442accca15f0fae2cc2444cb4e
Brook Elgie <specialbrew@gmail.com>
2016-02-10 11:50:42 +0000

    Heroku ready

--- rtei/settings/production.py
+++ rtei/settings/production.py
@@ -3,6 +3,13 @@ from .base import *
 
 DEBUG = False
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
 try:
     from .local import *
 except ImportError:
--- rtei/wsgi.py
+++ rtei/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rtei.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rtei.settings.production")
 
 application = get_wsgi_application()
