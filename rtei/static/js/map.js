b99a7f6d5311385c4eabbd995208c0c8dde22b4d
amercader <amercadero@gmail.com>
2016-02-12 13:05:04 +0000

    Refactor map js, turn it into a module

--- rtei/static/js/map.js
+++ rtei/static/js/map.js
@@ -1,57 +1,54 @@
-$(document).ready(function(){
+var RTEI = RTEI || {}
+RTEI.map = (function() {
 
   var homepage = window.location.href.indexOf('explore/map') === -1;
 
-  var map = L.map('map', {
-      attributionControl: false
-  }).setView([10, 0], 2);
-
   // get color depending on the selected index
   function getColor(score) {
-      if (!score) {
-        return '#B2B2B2';
-      }
-      score = parseFloat(score);
-      return score > 80 ? '#E55066' :
-             score > 60 ? '#D21E43' :
-             score > 40 ? '#8D1423' :
-             score > 20 ? '#48130B' :
-             score > 0  ? '#120E05' :
-                      '#B2B2B2';
+    if (!score) {
+      return '#B2B2B2';
+    }
+    score = parseFloat(score);
+    return score > 80 ? '#E55066' :
+           score > 60 ? '#D21E43' :
+           score > 40 ? '#8D1423' :
+           score > 20 ? '#48130B' :
+           score > 0  ? '#120E05' :
+                    '#B2B2B2';
   }
 
   function style(feature) {
-      return {
-          weight: 1,
-          opacity: 1,
-          color: 'white',
-          fillOpacity: 1,
-          fillColor: getColor(feature.properties.index)
-      };
+    return {
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.index)
+    };
   }
 
   function highlightFeature(e) {
-      var layer = e.target;
-      layer.setStyle({
-          weight: 2,
-          color: '#666',
-          dashArray: '',
-          fillOpacity: 0.7
-      });
+    var layer = e.target;
+    layer.setStyle({
+      weight: 2,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
 
-      if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-      }
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
+    }
   }
 
-  var geojson;
+  var geoJSONLayer;
 
   function resetHighlight(e) {
-      geojson.resetStyle(e.target);
+    geoJSONLayer.resetStyle(e.target);
   }
 
   function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
+    RTEI.map.map.fitBounds(e.target.getBounds());
   }
 
   function onEachFeature(feature, layer) {
@@ -69,17 +66,40 @@ $(document).ready(function(){
       className: 'rtei-map-popup'}
     );
     layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight
+      mouseover: highlightFeature,
+      mouseout: resetHighlight
     });
   }
 
-  geoJsonLayer = L.geoJson(null, {
+  var customGeoJSONLayer = L.geoJson(null, {
       style: style,
       onEachFeature: onEachFeature
   });
 
-  var geojson = omnivore.topojson('/static/data/countries.topojson', null , geoJsonLayer)
-      .addTo(map);
 
+  return {
+
+    // The actual Leaflet map object
+    map: null,
+
+    // The index to display ('index' for the overall score, '1', '1.1', etc otherwise)
+    currentIndex: 'index',
+
+    init: function() {
+
+      RTEI.map.map = L.map('map', {
+          attributionControl: false
+      }).setView([10, 0], 2);
+
+      geoJSONLayer = omnivore.topojson('/static/data/countries.topojson', null , customGeoJSONLayer)
+        .addTo(RTEI.map.map);
+
+    }
+  }
+
+
+})()
+
+$(document).ready(function(){
+    RTEI.map.init();
 });
