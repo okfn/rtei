943636a3e3d0af2b5d3eff4423653149fb8427f1
Sam Smith <sam@mintcanary.com>
2016-02-15 19:45:30 +0000

    map switcher styling

--- rtei/static/js/rtei.js
+++ rtei/static/js/rtei.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
   //nav
   $( '#smenu li:has(ul)' ).doubleTapToGo();
+
+
+  //map switcher
+  $('.indicator-switcher input').click(function () {
+    $("li").removeClass("subcat");
+  });
+
+  $('.indicator-switcher input:checked').closest("ul").closest("li").addClass("subcat");
+
+  $('.indicator-switcher ul ul input').click(function () {
+    $(this).closest("ul").closest("li").addClass("subcat");
+  });
 });
