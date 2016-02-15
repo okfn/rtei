48b1cffed43140968c787576f0afe3318fca57db
Brook Elgie <specialbrew@gmail.com>
2016-02-15 12:32:47 +0000

    AWS_S3_HOST env var is optional.
    Not needed if S3 host is US default.

--- rtei/settings/production.py
+++ rtei/settings/production.py
@@ -18,7 +18,8 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Necessary to overcome broken pipe error if not default US location
 # (https://github.com/boto/boto/issues/621).
-AWS_S3_HOST = os.environ.get('AWS_S3_HOST')
+if os.environ.get('AWS_S3_HOST', False):
+    AWS_S3_HOST = os.environ.get('AWS_S3_HOST')
 MEDIA_URL = "https://%s/" % (AWS_S3_CUSTOM_DOMAIN)
 
 ALLOWED_HOSTS = [
