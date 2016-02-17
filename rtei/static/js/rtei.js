ea896e1b807332be7902c2a2145402f84f52dca1
Sam Smith <sam@mintcanary.com>
2016-02-17 15:12:35 +0000

    expand map filters on click

--- rtei/static/js/rtei.js
+++ rtei/static/js/rtei.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  //nav
+  // nav
   $( '#smenu li:has(ul)' ).doubleTapToGo();
 
 
-  //map switcher
+  // map switcher
+  // parent class
   $('.indicator-switcher input').click(function () {
     $("li").removeClass("subcat");
   });
@@ -13,4 +14,12 @@ $(document).ready(function() {
   $('.indicator-switcher ul ul input').click(function () {
     $(this).closest("ul").closest("li").addClass("subcat");
   });
+
+  // slide
+  $( ".sidebar-layout .indicator-switcher h6" ).next( "ul" ).slideUp( "fast");
+  $( ".sidebar-layout .indicator-switcher h6" ).click(function() {
+    $( this ).next( "ul" ).slideToggle();
+    $( this ).toggleClass( "expanded" );
+  });
+
 });
