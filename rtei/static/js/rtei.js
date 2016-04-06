6453e9daa8655db38452c490c834415cf4a7f7d1
Sam Smith <sam@mintcanary.com>
2016-04-06 09:56:27 +0100

    open filter when label is clicked (WIP)

--- rtei/static/js/rtei.js
+++ rtei/static/js/rtei.js
@@ -17,9 +17,15 @@ $(document).ready(function() {
 
   // slide
   $( ".indicator-switcher h6" ).next( "ul" ).slideUp( "fast");
-  $( ".indicator-switcher h6" ).click(function() {
-    $( this ).next( "ul" ).slideToggle();
-    $( this ).toggleClass( "expanded" );
+
+  $( ".indicator-switcher label" ).click(function() {
+    $( ".indicator-switcher li:not(.subcat) input:not(:checked) + label + h6" ).removeClass( "expanded" );
+    $( ".indicator-switcher li:not(.subcat) input:not(:checked) + label + h6 + ul" ).slideUp();
+  });
+
+  $( ".indicator-switcher h6, .indicator-switcher label" ).click(function() {
+    $( this ).parent().children( "ul" ).slideToggle();
+    $( this ).parent().children("h6").toggleClass( "expanded" );
   });
 
 });
